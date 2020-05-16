7c867a1e29797056478bfacdb2f610e158d25b30
Omeed <omeed.rabbani@gmail.com>
2020-05-16 15:52:29 -0700

    Update md5crack.py

--- md5crack.py
+++ md5crack.py
@@ -1,6 +1,7 @@
 from hashlib import md5
 from sys import exit
 import argparse, time
+#import chardet
 
 parser = argparse.ArgumentParser(
     description='\
@@ -29,36 +30,46 @@ def get_file_as_list(path):
         file.close()
         # Remove empty lines
         while('' in lst): lst.remove('')
+    # Remove carriage returns
+    for i in range(len(lst)):
+        if '\r' in lst[i]:
+            lst[i] = lst[i][:-1]
     return lst
 
-hashes = get_file_as_list(args.HASHFILE)
-print('Number of hashes to test: ' + str(len(hashes)))
+hashlist = get_file_as_list(args.HASHFILE)
+
+print('Number of hashes to test: ' + str(len(hashlist)))
 
 dictionary = get_file_as_list(args.DICTIONARY)
+#encoding = chardet.detect(dictionary[0])['encoding']
+#print('Dictionary encoding: ' + encoding)
 print('Number of words in dictionary: ' + str(len(dictionary)) + '\n')
 
 start_time = time.time()
 print('Starting on ' + time.ctime(start_time) + '\n')
 
 found = 0
-i = 0
+hashed = 0
+invalid = 0
 for word in dictionary:
-    i += 1
     try:
-        md5hash = md5(word.encode('ascii')).hexdigest()
-    except UnicodeEncodeError:
+        md5hash = md5(word).hexdigest()
+        hashed += 1
+    except:
         print('Invalid dictionary entry: ' + word)
+        invalid += 1
     else:
-        if md5hash in hashes:
+        if md5hash in hashlist:
             print(word+'::'+md5hash)
             found += 1
 
-    if found == len(hashes): 
+    if found == len(hashlist): 
         print('All hashes found.')
         break
 
 end_time = time.time()
 print('\nFinished on ' + time.ctime(end_time))
 print('Time taken: %i seconds' % (end_time - start_time))
-print('Words hashed: %i' % i)
+print('Words hashed: %i' % hashed)
+print('Invalid dictionary entries: %i' % invalid)
 print('Valid hashes found: %i' % found)
