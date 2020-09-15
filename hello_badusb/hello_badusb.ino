b11f65f4b16510a024b0df45b89880cc718c30be
Omeed <omeed.rabbani@gmail.com>
2020-09-15 16:31:38 -0700

    Update hello_badusb.ino

--- hello_badusb/hello_badusb.ino
+++ hello_badusb/hello_badusb.ino
@@ -17,14 +17,17 @@ const int dlong = 500;
 const int dshort = 50;
 
 // This is the payload PowerShell will execute
-const char[] payload = "Set-Content -Path \"C:\\hello.txt\" -Value \"hello, world!\" -Force"
+const char payload[] = "Set-Content -Path \"C:\\hello.txt\" -Value \"hello, world!\" -Force";
 
 void setup() {
+  // INIT
   Keyboard.begin();
   delay(dlong);
+
+  // PHASE 1: Delivery
   
   // Open PowerShell as Administrator
-  Keyboard.write(KEY_LEFT_GUI); // Windows key
+  Keyboard.write(KEY_LEFT_GUI);
   delay(dlong);
   Keyboard.print("powershell");
   delay(dlong);
@@ -47,6 +50,39 @@ void setup() {
 
   // Close PowerShell
   Keyboard.println("exit");
+  
+  // END PHASE 1
+
+  // Pause between phases
+  delay(dlong*2);
+
+  // PHASE 2: Cleanup
+  
+  // Open File Explorer to C drive
+  Keyboard.write(KEY_LEFT_GUI);
+  delay(dlong);
+  Keyboard.print("C:\\");
+  delay(dshort);
+  Keyboard.write(KEY_RETURN);
+  delay(dlong);
+
+  // Select and delete file
+  Keyboard.write(KEY_DOWN_ARROW);
+  delay(dshort);
+  Keyboard.print("hello.txt");
+  delay(dshort);
+  Keyboard.write(KEY_DELETE);
+  delay(dlong);
+  Keyboard.write(KEY_RETURN);
+  delay(dlong*4);
+
+  // Close File Explorer
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F4);
+  delay(dshort);
+  Keyboard.releaseAll();
+  
+  // END PHASE 2
 }
 
 void loop() {
